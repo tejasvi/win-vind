203b095a4a094c16a09f4405a5b1ebad20ac996e
pit-ray <pit-ray@outlook.com>
2020-04-28 05:39:19 +0900

    remove unuse dir

--- release/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/K-PC/GoogleDrive/GitHub/win-vind
-# Build directory: C:/Users/K-PC/GoogleDrive/GitHub/win-vind/release
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- release/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/K-PC/GoogleDrive/GitHub/win-vind/src
-# Build directory: C:/Users/K-PC/GoogleDrive/GitHub/win-vind/release/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
