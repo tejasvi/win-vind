c9feb8f236d27401509362ece54ff4428fbe19e0
pit-ray <pit-ray@outlook.com>
2020-05-05 08:31:10 +0900

    remove extra directory

--- build/src/CMakeFiles/EditorKeybinds.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for EditorKeybinds.
-# This may be replaced when dependencies are built.
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/pit-r/GoogleDrive/GitHub/win-vind/src
-# Build directory: C:/Users/pit-r/GoogleDrive/GitHub/win-vind/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
