eed020b69ecae49912270a1eda00576e4491d820
pit-ray <pit-ray@outlook.com>
2020-05-05 08:29:46 +0900

    change an architecture of IntervalTimer to Pimpl

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/pit-r/GoogleDrive/GitHub/win-vind
+# Build directory: C:/Users/pit-r/GoogleDrive/GitHub/win-vind/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ build/src/CMakeFiles/Keybinds.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Keybinds.
+# This may be replaced when dependencies are built.
