699af3a2672268a5966fe569631cb08dfcff0528
pit-ray <pit-ray@outlook.com>
2020-05-23 01:46:27 +0900

    add OpenNewCurrentWindow, MakeDir, OpenOtherFile, ReloadCurrentWindow

---
+++ build/src/system/CMakeFiles/EditorKeybinds.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/src/system/editor_keybinds/edi_change_mode.cpp" "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/build/src/system/CMakeFiles/EditorKeybinds.dir/editor_keybinds/edi_change_mode.cpp.obj"
+  "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/src/system/editor_keybinds/edi_jump_caret.cpp" "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/build/src/system/CMakeFiles/EditorKeybinds.dir/editor_keybinds/edi_jump_caret.cpp.obj"
+  "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/src/system/editor_keybinds/edi_move_caret.cpp" "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/build/src/system/CMakeFiles/EditorKeybinds.dir/editor_keybinds/edi_move_caret.cpp.obj"
+  "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/src/system/editor_keybinds/edi_search_pattern.cpp" "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/build/src/system/CMakeFiles/EditorKeybinds.dir/editor_keybinds/edi_search_pattern.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/system"
+  "C:/boost"
+  "../src/system/../../include/system"
+  "../src/system/../../include/system/common"
+  "../src/system/../../include/system/keybinds"
+  "../src/system/../../include/system/editor_keybinds"
+  "../src/system/../../include/system/options"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/K-PC/GoogleDrive/GitHub/win-vind/build/src/system/CMakeFiles/Common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
