f02255069a5ae05d5a2494e3845acfc89c2e482d
pit-ray <pit-ray@outlook.com>
2020-07-21 11:32:52 +0900

    add O, I/gI, A Commands

---
+++ debug/CMakeFiles/win-vind.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "resource.rc.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/ui_translator.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/pref_parser.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wx_system_tray.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wx_prop_dlg.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wx_settings.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wx_bind_list.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wx_shortcut_apps.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wx_options.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/wxgui.cpp.obj"
+  "CMakeFiles/win-vind.dir/src/wxgui/main.cpp.obj"
+  "win-vind.pdb"
+  "win-vind.exe"
+  "win-vind.exe.manifest"
+  "libwin-vind.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/win-vind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
